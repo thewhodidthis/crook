159ec3f818919ba7db765ba6dd819dfb3b787423
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-09-15 07:10:13 +0200

    paste in place

--- example/index.js
+++ example/index.js
@@ -3,15 +3,14 @@
 var html = document.documentElement;
 
 var canvas = document.getElementById('canvas');
-var target = canvas.getContext('2d');
+var source = canvas.getContext('2d');
+var lookup = canvas.cloneNode().getContext('2d');
 
-var lookup = document.createElement('canvas').getContext('2d');
-var source = document.createElement('canvas').getContext('2d');
-var sprite = document.createElement('img');
+var target = source.getImageData(0, 0, canvas.width, canvas.height);
+var master = document.createElement('img');
 
 var workerBlob = new Blob([document.getElementById('worker').textContent]);
 var workerBlobUrl = (window.URL || window.webkitURL).createObjectURL(workerBlob);
-
 var worker = new Worker(workerBlobUrl);
 
 var isOn = false;
@@ -22,21 +21,16 @@ var size = {
   y: 1
 };
 
-var targetData = target.getImageData(0, 0, canvas.width, canvas.height);
-var targetPixels = targetData.data;
-
 html.className = 'html';
 
 if (window !== window.top) {
   html.className += ' is-iframe';
 }
 
-lookup.canvas.width = source.canvas.width = canvas.width;
-lookup.canvas.height = source.canvas.height = canvas.height;
-
 lookup.fillStyle = '#808080';
-lookup.fillRect(0, 0, lookup.canvas.width, lookup.canvas.height);
+lookup.fillRect(0, 0, canvas.width, canvas.height);
 
+// TODO: draw random lines instead
 for (var row = 0; row < rows; row += 1) {
   for (var col = 0; col < cols; col += 1) {
     var x = col * size.x;
@@ -64,28 +58,25 @@ canvas.addEventListener('click', function _onClick(e) {
   e.preventDefault();
 
   if (isOn) {
-    target.drawImage(lookup.canvas, 0, 0);
+    source.drawImage(lookup.canvas, 0, 0);
 
     isOn = false;
   } else {
-    targetData.data.set(targetPixels);
-    target.putImageData(targetData, 0, 0);
+    source.putImageData(target, 0, 0);
 
     isOn = true;
   }
 }, false);
 
 worker.addEventListener('message', function _onDoneProcessing(e) {
-  targetPixels = e.data.result;
-
-  targetData.data.set(targetPixels);
-  target.putImageData(targetData, 0, 0);
+  target.data.set(e.data.result);
+  source.putImageData(target, 0, 0);
 
   isOn = true;
 }, false);
 
-sprite.addEventListener('load', function _onLoad(e) {
-  source.drawImage(sprite, 0, 0);
+master.addEventListener('load', function _onLoad(e) {
+  source.drawImage(master, 0, 0);
 
   worker.postMessage({
     source: source.getImageData(0, 0, canvas.width, canvas.height),
@@ -93,5 +84,5 @@ sprite.addEventListener('load', function _onLoad(e) {
   });
 }, false);
 
-sprite.setAttribute('src', '/sprite.jpg');
+master.setAttribute('src', '/sprite.jpg');
 
