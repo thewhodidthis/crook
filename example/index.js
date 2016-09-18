9fd28018b0903f23be642b1a5d1409aa2cda3709
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-09-18 22:35:49 +0200

    use source image as the displacement map

--- example/index.js
+++ example/index.js
@@ -5,17 +5,14 @@ var html = document.documentElement;
 var canvas = document.getElementById('canvas');
 var source = canvas.getContext('2d');
 var lookup = canvas.cloneNode().getContext('2d');
-var target = source.getImageData(0, 0, canvas.width, canvas.height);
 var master = document.createElement('img');
+var output = source.getImageData(0, 0, canvas.width, canvas.height);
 
 var workerBlob = new Blob([document.getElementById('worker').textContent]);
 var workerBlobUrl = (window.URL || window.webkitURL).createObjectURL(workerBlob);
 var worker = new Worker(workerBlobUrl);
 
 var isOn = 0;
-var getRandom = function(max) {
-  return Math.floor(Math.random() * max);
-};
 
 html.className = 'html';
 
@@ -23,49 +20,25 @@ if (window !== window.top) {
   html.className += ' is-iframe';
 }
 
-lookup.fillStyle = '#808080';
-lookup.fillRect(0, 0, canvas.width, canvas.height);
-
-for (var l = 0; l < 100; l += 1) {
-  var x1 = getRandom(canvas.width);
-  var x2 = getRandom(canvas.width);
-  var y1 = getRandom(canvas.height);
-  var y2 = getRandom(canvas.height);
-
-  if (l % 2 === 0) {
-    lookup.strokeStyle = '#f00';
-  } else {
-    lookup.strokeStyle = '#0f0';
-  }
-
-  lookup.lineWidth = getRandom(5) + 1;
-
-  lookup.beginPath();
-  lookup.moveTo(x1, y1);
-  lookup.lineTo(x2, y2);
-  lookup.stroke();
-}
-
 canvas.addEventListener('click', function _onClick(e) {
   e.preventDefault();
 
-  if (isOn === 0) {
-    source.putImageData(target, 0, 0);
-  } else if (isOn === 1) {
-    source.drawImage(lookup.canvas, 0, 0);
-  } else if (isOn === 2) {
+  if (isOn) {
     source.drawImage(master, 0, 0);
+  } else {
+    source.putImageData(output, 0, 0);
   }
 
-  isOn = (isOn + 1) % 3;
+  isOn = !isOn;
 }, false);
 
 worker.addEventListener('message', function _onDoneProcessing(e) {
-  target = e.data.result;
+  output.data.set(e.data.result);
 }, false);
 
 master.addEventListener('load', function _onLoad(e) {
   source.drawImage(master, 0, 0);
+  lookup.drawImage(master, 0, 0);
 
   worker.postMessage({
     source: source.getImageData(0, 0, canvas.width, canvas.height),
