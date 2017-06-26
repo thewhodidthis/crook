7536abb849d010e57a51f99e3d27f4c6dde6431e
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-26 20:10:24 +0200

    example: cleanup

--- example/index.es
+++ example/index.es
@@ -1,9 +1,9 @@
 const canvas = document.querySelector('canvas');
-const source = canvas.getContext('2d');
+const master = canvas.getContext('2d');
 const lookup = canvas.cloneNode().getContext('2d');
 
-const master = document.createElement('img');
-const output = source.getImageData(0, 0, canvas.width, canvas.height);
+const source = document.createElement('img');
+const output = master.getImageData(0, 0, canvas.width, canvas.height);
 
 const workerBlob = new Blob([document.getElementById('worker').textContent]);
 const workerBlobUrl = (window.URL || window.webkitURL).createObjectURL(workerBlob);
@@ -19,9 +19,9 @@ canvas.addEventListener('click', (e) => {
   e.preventDefault();
 
   if (toggle) {
-    source.drawImage(master, 0, 0);
+    master.drawImage(source, 0, 0);
   } else {
-    source.putImageData(output, 0, 0);
+    master.putImageData(output, 0, 0);
   }
 
   toggle = !toggle;
@@ -31,15 +31,15 @@ worker.addEventListener('message', (e) => {
   output.data.set(e.data.result.data);
 });
 
-master.addEventListener('load', () => {
-  source.drawImage(master, 0, 0);
-  lookup.drawImage(master, 0, 0);
+source.addEventListener('load', () => {
+  master.drawImage(source, 0, 0);
+  lookup.drawImage(source, 0, 0);
 
   worker.postMessage({
-    source: source.getImageData(0, 0, canvas.width, canvas.height),
+    source: master.getImageData(0, 0, canvas.width, canvas.height),
     lookup: lookup.getImageData(0, 0, canvas.width, canvas.height),
   });
 });
 
-master.setAttribute('src', '/master.jpg');
+source.setAttribute('src', 'master.jpg');
 
--- example/index.js
+++ example/index.js
@@ -2,11 +2,11 @@
 'use strict';
 
 var canvas = document.querySelector('canvas');
-var source = canvas.getContext('2d');
+var master = canvas.getContext('2d');
 var lookup = canvas.cloneNode().getContext('2d');
 
-var master = document.createElement('img');
-var output = source.getImageData(0, 0, canvas.width, canvas.height);
+var source = document.createElement('img');
+var output = master.getImageData(0, 0, canvas.width, canvas.height);
 
 var workerBlob = new Blob([document.getElementById('worker').textContent]);
 var workerBlobUrl = (window.URL || window.webkitURL).createObjectURL(workerBlob);
@@ -22,9 +22,9 @@ canvas.addEventListener('click', function (e) {
   e.preventDefault();
 
   if (toggle) {
-    source.drawImage(master, 0, 0);
+    master.drawImage(source, 0, 0);
   } else {
-    source.putImageData(output, 0, 0);
+    master.putImageData(output, 0, 0);
   }
 
   toggle = !toggle;
@@ -34,16 +34,16 @@ worker.addEventListener('message', function (e) {
   output.data.set(e.data.result.data);
 });
 
-master.addEventListener('load', function () {
-  source.drawImage(master, 0, 0);
-  lookup.drawImage(master, 0, 0);
+source.addEventListener('load', function () {
+  master.drawImage(source, 0, 0);
+  lookup.drawImage(source, 0, 0);
 
   worker.postMessage({
-    source: source.getImageData(0, 0, canvas.width, canvas.height),
+    source: master.getImageData(0, 0, canvas.width, canvas.height),
     lookup: lookup.getImageData(0, 0, canvas.width, canvas.height)
   });
 });
 
-master.setAttribute('src', '/master.jpg');
+source.setAttribute('src', 'master.jpg');
 
 }());
