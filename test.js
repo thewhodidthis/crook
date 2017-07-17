e7384479f0dc4cd96a67f8811127984fd76878ba
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-17 21:49:13 +0200

    test: move one dir up, relint

---
+++ test.js
@@ -0,0 +1,19 @@
+require('kpow')()
+
+const test = require('tape')
+const createFilter = require('./')
+
+test('will operate', (t) => {
+  const source = new ImageData(1, 1)
+  const lookup = new ImageData(2, 2)
+
+  const filter = createFilter()
+
+  t.equal(typeof filter, 'function', 'returns funtion on init')
+
+  const result = filter(source, lookup)
+
+  t.ok(result instanceof source.constructor, 'input/output type is a match')
+  t.equal(result.data.length, source.data.length, 'input/output size is a match')
+  t.end()
+})
