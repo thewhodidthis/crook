95c308f82812a3b479689981f70fe018c0888b8c
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-21 14:21:00 +0200

    test: check for defaults

--- test.js
+++ test.js
@@ -3,17 +3,23 @@ require('kpow')()
 const test = require('tape')
 const createFilter = require('./')
 
+test('will default', (t) => {
+  const filter = createFilter()
+  const result = filter()
+
+  t.equal(typeof filter, 'function', 'returns funtion on init')
+  t.equal(result.data.length, 4)
+  t.end()
+})
+
 test('will operate', (t) => {
   const source = new ImageData(1, 1)
   const lookup = new ImageData(2, 2)
 
   const filter = createFilter()
-
-  t.equal(typeof filter, 'function', 'returns funtion on init')
-
   const result = filter(source, lookup)
 
   t.ok(result instanceof source.constructor, 'input/output type is a match')
-  t.equal(result.data.length, source.data.length, 'input/output size is a match')
+  t.equal(result.data.length, lookup.data.length, 'lookup/output size is a match')
   t.end()
 })
