a43f6060f328f8c86b61699580864c56d727c5b7
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-11-01 11:08:50 +0100

    test: go tapeless

--- test.js
+++ test.js
@@ -1,25 +1,20 @@
-require('kpow')()
+import 'cutaway'
+import { assert, report } from 'tapeless'
+import createFilter from './index.es'
 
-const test = require('tape')
-const createFilter = require('./')
+const { ok, equal } = assert
 
-test('will default', (t) => {
-  const filter = createFilter()
-  const result = filter()
+const filter = createFilter()
+const { data } = filter()
 
-  t.equal(typeof filter, 'function', 'returns funtion on init')
-  t.equal(result.data.length, 4)
-  t.end()
-})
+equal(typeof filter, 'function', 'returns funtion on init', 'will default')
+equal(data.length, 4)
 
-test('will operate', (t) => {
-  const source = new ImageData(1, 1)
-  const lookup = new ImageData(2, 2)
+const source = new ImageData(1, 1)
+const lookup = new ImageData(2, 2)
+const result = filter(source, lookup)
 
-  const filter = createFilter()
-  const result = filter(source, lookup)
+ok(result instanceof source.constructor, 'input/output type is a match', 'will operate')
+equal(result.data.length, lookup.data.length, 'lookup/output size is a match')
 
-  t.ok(result instanceof source.constructor, 'input/output type is a match')
-  t.equal(result.data.length, lookup.data.length, 'lookup/output size is a match')
-  t.end()
-})
+report()
