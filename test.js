3604ee0b3ea0b4908a0de195891ce0d093c0b734
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-01-03 13:34:49 +0100

    test: update per lates amends to run within nodejs

--- test.js
+++ test.js
@@ -1,20 +1,16 @@
-import 'cutaway'
-import { assert, report } from 'tapeless'
-import createFilter from './index.es'
+'use strict'
 
-const { ok, equal } = assert
+const { equal } = require('tapeless')
+const createFilter = require('./')
 
 const filter = createFilter()
 const { data } = filter()
 
-equal(typeof filter, 'function', 'returns funtion on init', 'will default')
-equal(data.length, 4)
+equal(typeof filter, 'function', 'returns lambda on init', 'will default')
+equal(data.length, 0)
 
-const source = new ImageData(1, 1)
-const lookup = new ImageData(2, 2)
+const source = { data: [1, 2, 3, 4] }
+const lookup = { data: [5, 6, 7, 8] }
 const result = filter(source, lookup)
 
-ok(result instanceof source.constructor, 'input/output type is a match', 'will operate')
-equal(result.data.length, lookup.data.length, 'lookup/output size is a match')
-
-report()
+equal(result.data.length, source.data.length, 'input/output size is a match', 'will operate')
