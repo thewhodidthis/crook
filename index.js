99f9d8946690a8cb6347e59765ff645e2d1801d9
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-21 06:42:43 +0100

    module: buble not the cjs version

--- index.js
+++ index.js
@@ -1,56 +1,47 @@
 'use strict';
 
 // Three ways of handling excess
-var clamp = function (v, max) { return (v >= max ? max - 1 : Math.max(v, 0)); };
-var pleat = function (v, max) { return (v >= max || v < 0 ? v + (max * Math.sign(v)) : v); };
-var skirt = function (v, max) { return (v >= max || v < 0 ? NaN : v); };
+const clamp = (v, max) => (v >= max ? max - 1 : Math.max(v, 0));
+const pleat = (v, max) => (v >= max || v < 0 ? v + (max * Math.sign(v)) : v);
+const skirt = (v, max) => (v >= max || v < 0 ? NaN : v);
 
 // Color shift above and below gray
-var shift = function (color, scale) {
-  var ratio = scale * (color - 128) / 256;
+const shift = (color, scale) => {
+  const ratio = scale * (color - 128) / 256;
 
   return Math.sign(ratio) * Math.round(Math.abs(ratio))
 };
 
 // Extract options for color channel, scaling across each axis, edge work type
-var crook = function (ref) {
-  if ( ref === void 0 ) ref = {};
-  var channel = ref.channel; if ( channel === void 0 ) channel = { x: 0, y: 0 };
-  var scale = ref.scale; if ( scale === void 0 ) scale = channel;
-  var mode = ref.mode; if ( mode === void 0 ) mode = 0;
-
+const crook = ({ channel = { x: 0, y: 0 }, scale = channel, mode = 0 } = {}) => {
   // Wrap how?
-  var round = mode ? mode === 1 ? clamp : pleat : skirt;
+  const round = mode ? mode === 1 ? clamp : pleat : skirt;
 
   // Expects and returns an `ImageData` like object plus a color map of similar type
-  return function (target, lookup) {
-    if ( target === void 0 ) target = { data: [], width: 0, height: 0 };
-    if ( lookup === void 0 ) lookup = target;
-
-    var bitmap = new Int32Array(target.data.buffer);
-    var mirror = new Int32Array(bitmap);
+  return (target = { data: [], width: 0, height: 0 }, lookup = target) => {
+    const bitmap = new Int32Array(target.data.buffer);
+    const mirror = new Int32Array(bitmap);
 
     // Extract color map dimensions
-    var w = lookup.width;
-    var h = lookup.height;
+    const { width: w, height: h } = lookup;
 
-    for (var i = 0, stop = w * h; i < stop; i += 1) {
+    for (let i = 0, stop = w * h; i < stop; i += 1) {
       // In steps of four - r/g/b/a
-      var j = i * 4;
+      const j = i * 4;
 
       // Calculate pixel coords
-      var x = i % w;
-      var y = Math.floor(i / w);
+      const x = i % w;
+      const y = Math.floor(i / w);
 
       // Process coords
-      var x1 = x + shift(lookup.data[j + channel.x], scale.x);
-      var y1 = y + shift(lookup.data[j + channel.y], scale.y);
+      const x1 = x + shift(lookup.data[j + channel.x], scale.x);
+      const y1 = y + shift(lookup.data[j + channel.y], scale.y);
 
-      var x2 = round(x1, w);
-      var y2 = round(y1, h);
+      const x2 = round(x1, w);
+      const y2 = round(y1, h);
 
       // Find color map index at pixel coords
-      var k = x2 + (y2 * w);
+      const k = x2 + (y2 * w);
 
       // Replace input data with color map data at index, or a blank pixel if index negative
       bitmap[i] = k >= 0 ? mirror[k] : [0, 0, 0, 255];
@@ -61,4 +52,3 @@ var crook = function (ref) {
 };
 
 module.exports = crook;
-
