fac38c5b09d1f413b148264393c4e29758ba75e3
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-02-18 20:44:38 +0000

    test: tweak for tapeless latest

--- test.js
+++ test.js
@@ -6,11 +6,16 @@ const createFilter = require('./')
 const filter = createFilter()
 const { data } = filter()
 
-equal(typeof filter, 'function', 'returns lambda on init', 'will default')
-equal(data.length, 0)
+equal
+  .describe('returns lambda on init')
+  .test(typeof filter, 'function')
+  .describe('data empty', 'will default')
+  .test(data.length, 0)
 
 const source = { data: [1, 2, 3, 4] }
 const lookup = { data: [5, 6, 7, 8] }
 const result = filter(source, lookup)
 
-equal(result.data.length, source.data.length, 'input/output size is a match', 'will operate')
+equal
+  .describe('input/output size is a match', 'will operate')
+  .test(result.data.length, source.data.length)
