f9bb7b9e9f6f32c15837a1f38626992944ce6775
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-05-28 19:22:26 +0100

    example: rebuild

--- example/crook.js
+++ example/crook.js
@@ -1,67 +1,42 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.crook = factory());
-}(this, (function () { 'use strict';
-
-  // # Crook
-  // Helps with pixel shifting
-
-  // Clamps the displacement value to the edge of the source image
-  const clamp = (v, max) => (v >= max ? max - 1 : Math.max(v, 0));
-
-  // Wraps the displacement value to the other side of the source image
-  const pleat = (v, max) => (v >= max || v < 0 ? v + (max * Math.sign(v)) : v);
-
-  // Ignores the math and allows for using the source pixel if the displacement value is out of range
-  const skirt = (v, max) => (v >= max || v < 0 ? NaN : v);
-
-  // Color shift above and below gray
-  const shift = (color, scale) => {
-    const ratio = 0.00390625 * scale * (color - 128);
-
-    return Math.sign(ratio) * Math.round(Math.abs(ratio))
+var crook = (() => {
+  var __defProp = Object.defineProperty;
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
   };
 
-  // Extract options for color channel, scaling across each axis, edge work type
-  const crook = ({ channel = { x: 0, y: 0 }, scale = channel, mode = 0 } = {}) => {
-    // Wrap how?
+  // main.js
+  var main_exports = {};
+  __export(main_exports, {
+    default: () => main_default
+  });
+  var clamp = (v, max) => v >= max ? max - 1 : Math.max(v, 0);
+  var pleat = (v, max) => v >= max || v < 0 ? v + max * Math.sign(v) : v;
+  var skirt = (v, max) => v >= max || v < 0 ? NaN : v;
+  var shift = (color, scale) => {
+    const ratio = 390625e-8 * scale * (color - 128);
+    return Math.sign(ratio) * Math.round(Math.abs(ratio));
+  };
+  var crook = ({ channel = { x: 0, y: 0 }, scale = channel, mode = 0 } = {}) => {
     const round = mode ? mode === 1 ? clamp : pleat : skirt;
-
-    // Expects and returns an `ImageData` like object plus a color map of similar type
     return (target = { data: [], width: 0, height: 0 }, lookup = target) => {
       const bitmap = new Int32Array(target.data.buffer);
       const mirror = new Int32Array(bitmap);
-
-      // Extract color map dimensions
       const { width: w, height: h } = lookup;
-
       for (let i = 0, stop = w * h; i < stop; i += 1) {
-        // In steps of four - r/g/b/a
         const j = i * 4;
-
-        // Calculate pixel coords
         const x = i % w;
         const y = Math.floor(i / w);
-
-        // Process coords
         const x1 = x + shift(lookup.data[j + channel.x], scale.x);
         const y1 = y + shift(lookup.data[j + channel.y], scale.y);
-
         const x2 = round(x1, w);
         const y2 = round(y1, h);
-
-        // Find color map index at pixel coords
-        const k = x2 + (y2 * w);
-
-        // Replace input data with color map data at index, or a blank pixel if index negative or NaN
+        const k = x2 + y2 * w;
         bitmap[i] = k >= 0 ? mirror[k] : 0;
       }
-
-      return target
-    }
+      return target;
+    };
   };
-
-  return crook;
-
-})));
+  var main_default = crook;
+  return main_exports;
+})();
--- example/worker.js
+++ example/worker.js
@@ -1,7 +1,7 @@
-importScripts('crook.js')
+importScripts('./crook.js')
 
 self.addEventListener('message', (e) => {
-  const filter = crook(e.data.config)
+  const filter = crook.default(e.data.config)
 
   self.postMessage({
     result: filter(e.data.source, e.data.lookup)
